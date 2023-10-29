678659524213b027a53d5b228013a7e8996c1c51
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-10-29 08:59:31 +0300

    add style

--- src/Tiptap.tsx
+++ src/Tiptap.tsx
@@ -1,10 +1,15 @@
 // src/Tiptap.jsx
-import { useEditor, EditorProvider, FloatingMenu, BubbleMenu } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import MenuBar from './MenuBar'
-import { Color } from '@tiptap/extension-color'
-import ListItem from '@tiptap/extension-list-item'
-import TextStyle from '@tiptap/extension-text-style'
+import {
+  useEditor,
+  EditorProvider,
+  FloatingMenu,
+  BubbleMenu
+} from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import MenuBar from "./MenuBar";
+import { Color } from "@tiptap/extension-color";
+import ListItem from "@tiptap/extension-list-item";
+import TextStyle from "@tiptap/extension-text-style";
 
 const extensions = [
   Color.configure({ types: [TextStyle.name, ListItem.name] }),
@@ -12,30 +17,28 @@ const extensions = [
   StarterKit.configure({
     bulletList: {
       keepMarks: true,
-      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+      keepAttributes: false // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
     },
     orderedList: {
       keepMarks: true,
-      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
-    },
-  }),
-]
+      keepAttributes: false // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    }
+  })
+];
 
-const content = '<p>Hello World 2!</p>'
-
+const content = "<p>Hello World 2!</p>";
 
 const Tiptap = () => {
-  const editor = useEditor({
-    extensions,
-    content,
-  })
-
   return (
-    <EditorProvider slotBefore={<MenuBar />} extensions={extensions} content={content}>
+    <EditorProvider
+      slotBefore={<MenuBar />}
+      extensions={extensions}
+      content={content}
+    >
       <FloatingMenu>This is the floating menu</FloatingMenu>
-      {/* <BubbleMenu>This is the bubble menu</BubbleMenu> */}
+      <BubbleMenu>This is the bubble menu</BubbleMenu>
     </EditorProvider>
-  )
-}
+  );
+};
 
-export default Tiptap
+export default Tiptap;
